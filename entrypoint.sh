4777395b4046caf5fa73b1b475a6b762fa7d9f02
Keith Suderman <suderman@jhu.edu>
2022-08-15 17:17:05 -0400

    Initial code import

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu
+
+if [[ -e Chart.yaml ]] ; then
+  version=$(cat Chart.yaml | awk '/^version:/ { print $2 }')
+elif [[ -e VERSION ]] ; then
+  version=$(cat VERSION)
+else
+  echo "No version file found"
+  exit 1
+fi
+git config user.email $(git --no-pager log --format=format:'%ae' -n 1)
+git config user.name $(git --no-pager log --format=format:'%an' -n 1)
+git tag -a $version -m "Automatic release of v$version"
+git push origin $version
+gh release create $version --generate-notes --draft

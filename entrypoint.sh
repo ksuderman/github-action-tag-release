6c9ce2caf475ffdda29c0459d209469ad3775695
Keith Suderman <suderman@jhu.edu>
2022-08-16 11:41:15 -0400

    Better version file handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 set -eu
 
-if [[ -e Chart.yaml ]] ; then
-  version=$(cat Chart.yaml | awk '/^version:/ { print $2 }')
-elif [[ -e VERSION ]] ; then
-  version=$(cat VERSION)
+file=$1
+parser=$2
+draft=$3
+if [[ $@ == 4 ]] ; then
+  version=$4
 else
-  echo "No version file found"
-  exit 1
+  if [[ $parser == 'awk' ]] ; then
+    version=$(cat $file | awk '/^version:/ { print $2 }')
+  else
+    version=$(cat $file)
+  fi
+fi
+
+flags="--generate-notes"
+if [[ $draft == 'true' ]] ; then
+  flags="$flags --draft"
 fi
 git config --global --add safe.directory /github/workspace
 git config user.email $(git --no-pager log --format=format:'%ae' -n 1)
 git config user.name $(git --no-pager log --format=format:'%an' -n 1)
-git tag -a $version -m "Automatic release of v$version"
+git tag -a -m "Automatic release of v$version" $version
 git push origin $version
-gh release create $version --generate-notes --draft
+gh release create $version $flags
